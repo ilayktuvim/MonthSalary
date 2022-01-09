5cc90e69641f904d7b6b8c9269bd968b6edbc94f
ilay ktuvim <ilayktu@gmail.com>
2022-01-09 17:18:49 +0200

    my first version

---
+++ MonthSalary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonthSalary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = 8;
+
+            int days = 25;
+
+            double payment = 89.4;
+
+            double total = hours * days * payment;
+
+            Console.WriteLine("Salary :{0}",total);
+
+
+        }
+    }
+}
